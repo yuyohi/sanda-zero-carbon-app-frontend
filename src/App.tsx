9892297e8c3db57fdf5a4847d16e0678fa49d3e6
yuyohi <41873114+yuyohi@users.noreply.github.com>
2022-08-09 10:13:18 +0900

    feat: add navigationbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/material';
 import MainMenu from './components/main-menu/mainMenu';
+import FixedBottomNavigation from './components/navigationBar';
 import theme from './theme/theme';
 
 const App: FC = () => (
   <RecoilRoot>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <Box sx={{ position: 'fixed', left: 0, right: 0, bottom: 0 }}>
+          <FixedBottomNavigation />
+        </Box>
         <Routes>
           <Route path="/" element={<MainMenu />} />
         </Routes>
