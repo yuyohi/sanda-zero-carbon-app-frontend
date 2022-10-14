b7b8e47901fcb337164061c31722cdf3ce300c3f
yuyohi <41873114+yuyohi@users.noreply.github.com>
2022-10-14 11:40:30 +0900

    feat: customメニューを追加

--- src/containers/main-menu/mainMenu.tsx
+++ src/containers/main-menu/mainMenu.tsx
@@ -4,63 +4,50 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import TotalCondition from './totalCondition';
 import LevelStatus from './levelStatus';
-import ArticleView from '../../components/main-menu/articleView';
 import Map from './map';
 import MapNavigation from './mapNavigation';
+import CustomAppBar from '../../components/customAppBar';
 
-const MainMenu: FC = () => {
-  const articles = [
-    {
-      title: 'test_1',
-      date: new Date(2022, 8, 7, 10, 13),
-    },
-    {
-      title: 'test_2',
-      date: new Date(2022, 8, 6, 10, 13),
-    },
-  ];
-
-  return (
-    <Box sx={{ width: '100vw' }}>
-      <Box sx={{ position: 'fixed', top: 0, left: 0, right: 0 }}>
-        <span style={{ height: 20, display: 'block' }} />
-        <Grid container spacing={4}>
-          <Grid item xs={0.5}>
-            <span />
-          </Grid>
-          <Grid item xs={2.5}>
-            <LevelStatus />
-          </Grid>
-          <Grid item xs={8.5}>
-            <Box alignItems="center">
-              <ArticleView articles={articles} />
-            </Box>
-          </Grid>
-          <Grid item xs={0.5}>
-            <span />
-          </Grid>
+const MainMenu: FC = () => (
+  <Box sx={{ width: '100vw' }}>
+    <Box sx={{ position: 'fixed', top: 0, left: 0, right: 0 }}>
+      <span style={{ height: 20, display: 'block' }} />
+      <Grid container spacing={4}>
+        <Grid item xs={0.5}>
+          <span />
         </Grid>
-      </Box>
-      <Container sx={{ textAlign: 'center' }}>
-        <Map />
-      </Container>
-      <Box
-        sx={{
-          display: 'flex',
-          position: 'fixed',
-          left: 0,
-          right: 0,
-          bottom: '10vh',
-        }}
-      >
-        <div style={{ width: 10 }} />
-        <TotalCondition />
-        <div style={{ flexGrow: 1 }} />
-        <MapNavigation />
-        <div style={{ width: 10 }} />
-      </Box>
+        <Grid item xs={2.5}>
+          <LevelStatus />
+        </Grid>
+        <Grid item xs={8.5}>
+          <Box alignItems="center">
+            <CustomAppBar />
+          </Box>
+        </Grid>
+        <Grid item xs={0.5}>
+          <span />
+        </Grid>
+      </Grid>
     </Box>
-  );
-};
+    <Container sx={{ textAlign: 'center' }}>
+      <Map />
+    </Container>
+    <Box
+      sx={{
+        display: 'flex',
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        bottom: '10vh',
+      }}
+    >
+      <div style={{ width: 10 }} />
+      <TotalCondition />
+      <div style={{ flexGrow: 1 }} />
+      <MapNavigation />
+      <div style={{ width: 10 }} />
+    </Box>
+  </Box>
+);
 
 export default MainMenu;
