1e54616a00a50c95beeb3d67dfe4241970e5176e
nagatani-aoi <224x218x@stu.kobe-u.ac.jp>
2022-12-22 10:21:20 +0900

    記事のコンテンツの文字色を黒に

--- src/components/article/ArticleListView.tsx
+++ src/components/article/ArticleListView.tsx
@@ -15,7 +15,7 @@ import React from 'react';
 import {
   bodyArticleBigTypographyStyle,
   bodyArticleSmallTypographyStyle,
-  bodyArticleTypographyStyle,
+  // bodyArticleTypographyStyle,
   titleTypographyStyle,
 } from '../../utils/customStyles';
 import { Article } from '../../utils/TypeDefinition';
@@ -27,19 +27,21 @@ const ArticleCard = styled(Box)({
   backgroundColor: '#fffcd2',
   padding: '2%',
   width: '100%',
+  height: '100%',
 });
 
 const ArticleReadButton = styled('button')({
-  borderRadius: '5%',
+  // borderRadius: '5%',
   backgroundColor: '#F79428',
-  borderTop: '4px solid #C78B5D',
-  borderRight: '4px solid #946746',
-  borderBottom: '4px solid #946746',
-  borderLeft: '4px solid #C78B5D',
-  padding: '1%',
+  // borderTop: '4px solid #C78B5D',
+  // borderRight: '4px solid #946746',
+  // borderBottom: '4px solid #946746',
+  // borderLeft: '4px solid #C78B5D',
+  padding: '0%',
   margin: '1%',
-  width: '15%',
+  width: '100%',
   aspectRatio: '4 / 1',
+  justifyContent: 'center',
 });
 
 const ArticleListView = (props: { article: Article }) => {
@@ -59,89 +61,87 @@ const ArticleListView = (props: { article: Article }) => {
 
   return (
     <Grid item xs={12} key={article.title}>
-      <ArticleCard>
-        <Grid sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Grid item xs={4} sm={4}>
-            <img
-              src={article.thumbnailSource}
-              width="100%"
-              alt={article.title}
-            />
+      <ArticleReadButton onClick={() => handleClickAchieve()}>
+        <ArticleCard>
+          <Grid sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Grid item xs={3.5}>
+              <img
+                src={article.thumbnailSource}
+                width="100%"
+                alt={article.title}
+              />
+            </Grid>
+
+            <Grid item xs={7.5}>
+              <Typography
+                sx={{
+                  ...titleTypographyStyle,
+                  display: 'flex',
+                  justifyContent: 'left',
+                  color: 'black',
+                }}
+              >
+                {article.title}
+              </Typography>
+              <Divider />
+              <Typography
+                sx={{
+                  ...bodyArticleSmallTypographyStyle,
+                  display: 'flex',
+                  justifyContent: 'left',
+                }}
+              >
+                {article.postedAt.toString().split('T')[0]}
+              </Typography>
+              <Typography
+                sx={{
+                  ...bodyArticleBigTypographyStyle,
+                  display: 'flex',
+                  justifyContent: 'left',
+                  color: 'black',
+                }}
+              >
+                {article.description}
+              </Typography>
+            </Grid>
           </Grid>
+        </ArticleCard>
+      </ArticleReadButton>
 
-          <Grid item xs={7.5} sm={7.5}>
-            <Typography
-              sx={{
-                ...titleTypographyStyle,
-                display: 'flex',
-                justifyContent: 'left',
+      <Typography
+        sx={{
+          ...bodyArticleBigTypographyStyle,
+          display: 'flex',
+          justifyContent: 'right',
+        }}
+      >
+        <Dialog open={!!selectedArticle} onClose={handleCloseAchieve}>
+          <DialogTitle>確認</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              外部サイトへ飛びますが、よろしいですか？
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseAchieve} color="primary">
+              キャンセル
+            </Button>
+            <Button
+              onClick={() => {
+                if (selectedArticle) {
+                  const { url } = selectedArticle;
+                  window.open(url, '_blank');
+                  setSelectedArticle(null);
+                }
               }}
+              color="primary"
+              autoFocus
             >
-              {article.title}
-            </Typography>
-            <Divider />
-            <Typography
-              sx={{
-                ...bodyArticleSmallTypographyStyle,
-                display: 'flex',
-                justifyContent: 'left',
-              }}
-            >
-              {article.postedAt.toString().split('T')[0]}
-            </Typography>
-            <Typography
-              sx={{
-                ...bodyArticleBigTypographyStyle,
-                display: 'flex',
-                justifyContent: 'left',
-              }}
-            >
-              {article.description}
-            </Typography>
-
-            <Typography
-              sx={{
-                ...bodyArticleBigTypographyStyle,
-                display: 'flex',
-                justifyContent: 'right',
-              }}
-            >
-              <ArticleReadButton onClick={() => handleClickAchieve()}>
-                <Typography sx={{ ...bodyArticleTypographyStyle }}>
-                  読む
-                </Typography>
-              </ArticleReadButton>
-
-              <Dialog open={!!selectedArticle} onClose={handleCloseAchieve}>
-                <DialogTitle>確認</DialogTitle>
-                <DialogContent>
-                  <DialogContentText>
-                    外部サイトへ飛びますが、よろしいですか？
-                  </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                  <Button onClick={handleCloseAchieve} color="primary">
-                    キャンセル
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      if (selectedArticle) {
-                        const { url } = selectedArticle;
-                        window.open(url, '_blank');
-                        setSelectedArticle(null);
-                      }
-                    }}
-                    color="primary"
-                    autoFocus
-                  >
-                    はい
-                  </Button>
-                </DialogActions>
-              </Dialog>
-            </Typography>
-          </Grid>
-        </Grid>
-      </ArticleCard>
+              はい
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Typography>
     </Grid>
   );
 };
